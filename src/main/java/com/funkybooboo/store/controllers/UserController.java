06b76cecb4e177564f21b2e86a2fe158b4e9bd12
Nate Stott <nate.stott@pm.me>
2025-05-08 19:52:00 -0400

    dtos

---
+++ src/main/java/com/funkybooboo/store/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.funkybooboo.store.controllers;
+
+import com.funkybooboo.store.dtos.UserDto;
+import com.funkybooboo.store.entities.User;
+import com.funkybooboo.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping("/")
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll().stream().map(user -> new UserDto(user.getId(), user.getName(), user.getEmail())).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
+    }
+}
